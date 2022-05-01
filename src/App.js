d89ec8b3f4c0a126c0082120c9c2aa34099fbf7d
ToddMarino <todd.j.marino@gmail.com>
2022-05-01 17:44:23 -0500

    creating links
    adding router, routes, and route to app

--- src/App.js
+++ src/App.js
@@ -1,45 +1,53 @@
 import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
+import AboutPage from './pages/AboutPage'
 import FeedbackData from './data/FeedbackData'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
 
 /* SET STATE for user feedback */
 const App = () => {
-  const [feedback, setFeedback] = useState(FeedbackData)
-  
-// ADD AND DELETE FEEDBACK FUNCTIONS
-// deleteFeedback takes in id as parameter
-// add alert to confirm user wants to delete the feedback
-// set state: filter feedback array for the items that don't match the id
+	const [feedback, setFeedback] = useState(FeedbackData)
+
+	// ADD AND DELETE FEEDBACK FUNCTIONS
+	// deleteFeedback takes in id as parameter
+	// add alert to confirm user wants to delete the feedback
+	// set state: filter feedback array for the items that don't match the id
 	const deleteFeedback = (id) => {
 		if (window.confirm('Are you sure you want to delete?')) {
 			setFeedback(feedback.filter((item) => item.id !== id))
 		}
 	}
-// addFeedback takes in newFeedback as parameter
-// assign id to newFeedback object using uuid
-// set state: add newFeedback and spread in previous feedback
+	// addFeedback takes in newFeedback as parameter
+	// assign id to newFeedback object using uuid
+	// set state: add newFeedback and spread in previous feedback
 	const addFeedback = (newFeedback) => {
 		newFeedback.id = uuidv4()
 		setFeedback([newFeedback, ...feedback])
 	}
 
 	return (
-		<>
+		<Router>
 			<Header />
 			<div className='container'>
-				{/* pass handleAdd as prop called addFeedback to FeedbackForm component */}
-				<FeedbackForm handleAdd={addFeedback} />
-				{/* pass feedback object as prop called feedback to FeedbackStats component */}
-				<FeedbackStats feedback={feedback} />
-				{/* pass feedback object and handleDelete function as props called feedback */}
-				{/* and deleteFeedback to FeedbackList component */}
-				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+				<Routes>
+					<Route exact path='/' element={
+						<>
+						<FeedbackForm handleAdd={addFeedback} />
+						<FeedbackStats feedback={feedback} />
+						<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+						</>
+					}>
+					</Route>
+					<Route path='/about' element={<AboutPage />} />
+				</Routes>
+					<AboutIconLink />
 			</div>
-		</>
+		</Router>
 	)
 }
 export default App
