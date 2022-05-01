363a94acf1e8d558e8cbb7aa24b4f94b53c5d196
ToddMarino <todd.j.marino@gmail.com>
2022-04-30 20:47:59 -0500

    FeedbackRatings
    FeedbackRatings complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import {useState} from 'react'
+import Header from './components/Header'
+import FeedbackData from './data/FeedbackData'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+
 const App = () => {
+
+  const [feedback, setFeedback ] = useState(FeedbackData)
+
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id ))
+    }
+  }
+
   return (
-    <div>App Component</div>
+    <>
+    <Header  />
+    <div className="container">
+      <FeedbackStats feedback={feedback} />
+      <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+    </div>
+    </>
   )
 }
 export default App
