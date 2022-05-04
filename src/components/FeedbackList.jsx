8f30046cf390fcaef92b27d427544d71e20cd8f1
ToddMarino <todd.j.marino@gmail.com>
2022-05-04 12:11:07 -0500

    completed
    Finished context to fully use fetch api.

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,34 +2,33 @@ import { useContext } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import FeedbackItem from './FeedbackItem'
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 const FeedbackList = () => {
 
-	const {feedback} = useContext(FeedbackContext)
+	const {feedback, isLoading} = useContext(FeedbackContext)
 
-	if (!feedback || feedback.length === 0) {
+	if (!isLoading && (!feedback || feedback.length === 0)) {
 		return <p>No feedback yet ... </p>
 	}
-
-	return (
-		<div className='feedback-list'>
-			<AnimatePresence>
-				{feedback.map((item) => (
-					<motion.div
-						key={item.id}
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						exit={{ opacity: 0 }}
-					>
-						<FeedbackItem
-							key={item.id}
-							item={item}
-						/>
-					</motion.div>
-				))}
-			</AnimatePresence>
-		</div>
-	)
+return isLoading ? (
+	<Spinner />
+) : (
+	<div className='feedback-list'>
+		<AnimatePresence>
+			{feedback.map((item) => (
+				<motion.div
+					key={item.id}
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+				>
+					<FeedbackItem key={item.id} item={item} />
+				</motion.div>
+			))}
+		</AnimatePresence>
+	</div>
+)
 }
 
 export default FeedbackList
